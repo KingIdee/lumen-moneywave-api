f103ad5f908af1fda7965b71f6c4bc9ef182f6cc
Idorenyin Obong <idee4ril@gmail.com>
2017-09-18 22:14:25 +0100

    feat: get wallet balance

---
+++ resources/views/idee.blade.php
@@ -0,0 +1,7 @@
+<?php
+
+$headers = array('content-type' => 'application/json','Authorization'=>'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQwLCJuYW1lIjoic2F2YW5hIHNtYXJ0c2F2ZSIsImFjY291bnROdW1iZXIiOiIiLCJiYW5rQ29kZSI6Ijk5OSIsImlzQWN0aXZlIjp0cnVlLCJjcmVhdGVkQXQiOiIyMDE2LTEyLTA4VDEwOjM4OjE5LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDE3LTA2LTE0VDEzOjAxOjQ5LjAwMFoiLCJkZWxldGVkQXQiOm51bGwsImlhdCI6MTQ5ODMzNTE2NSwiZXhwIjoxNDk4MzQyMzY1fQ.WojvkYOC2j6XTUfg_E4WQkxQChPUyCgYUCIKaW83YXA');
+
+$response = Unirest\Request::get('https://moneywave.herokuapp.com/v1/wallet', $headers);
+
+echo $response->raw_body;
